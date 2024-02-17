ffde63fd724e5167c356bf984e88d58fec8a3082
Matheus-santos7 <matheussantosgc@icloud.com>
2024-02-17 12:53:53 -0300

    feat: login com o google

--- html/js/login.js
+++ html/js/login.js
@@ -82,53 +82,6 @@ async function handleSubmit(event) {
         exibirToast('Usuário não cadastrado', '#ff0000');
         document.querySelector("form").reset();
     }
-
     // desabilitarBotaoEntrar();
-}   
-
-//Login com facebook
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '361760433353788',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v13.0'
-    });
-      
-    FB.AppEvents.logPageView();   
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-
-  function loginWithFacebook() {
-    FB.login(function(response) {
-      if (response.authResponse) {
-        console.log('Usuário autorizado com sucesso!');
-        getUserInfo();
-      } else {
-        console.log('Usuário cancelou o login ou não autorizou a aplicação.');
-      }
-    }, {scope: 'email'}); // Solicita permissão de acesso ao email
-  }
-
-  function getUserInfo() {
-    FB.api('/me', {fields: 'id,name,email'}, function(response) {
-      console.log('Informações do usuário:', response);
-      // Aqui você pode enviar os dados do usuário para o seu servidor e criar uma sessão de login.
-    });
-
-  
-
-  }
-
-
-
-
+}
 
