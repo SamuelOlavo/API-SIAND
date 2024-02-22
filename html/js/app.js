e961fab6af4294f522c736583b7c49362d9ab20b
SamuelOlavo <samuelobt@gmail.com>
2024-02-21 23:51:17 -0300

    PROD

--- html/js/app.js
+++ html/js/app.js
@@ -1,9 +1,6 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const URL_TESTE = process.env.URL;
+export const URL_TESTE = "https://api-siand-esqe.onrender.com";
 
 export const clientID = "771987966504-vri95o8gkbvprv8rc3l4d1c30jfjhc0i.apps.googleusercontent.com";
 
-
+export const URL_PRODUCAO = "http://api-siand.vercel.app";
 
