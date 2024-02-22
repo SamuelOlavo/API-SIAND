fc190128d4d16c96356d15f9b205a9f89a9524da
SamuelOlavo <samuelobt@gmail.com>
2024-02-21 23:23:44 -0300

    URL PROD RENDER

--- html/js/app.js
+++ html/js/app.js
@@ -1,5 +1,5 @@
 
-export const URL_TESTE = "http://localhost:3000";
+export const URL_TESTE = "https://api-siand-esqe.onrender.com";
 
 export const clientID = "771987966504-vri95o8gkbvprv8rc3l4d1c30jfjhc0i.apps.googleusercontent.com";
 
