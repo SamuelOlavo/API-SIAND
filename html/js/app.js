e892dac078556830a2ada738e72a18b3c4323fcc
SamuelOlavo <samuelobt@gmail.com>
2024-02-21 23:44:15 -0300

    feat: implantação de variavel externa

--- html/js/app.js
+++ html/js/app.js
@@ -1,7 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const URL_TESTE = "https://api-siand-esqe.onrender.com";
+export const URL_TESTE = process.env.URL;
 
 export const clientID = "771987966504-vri95o8gkbvprv8rc3l4d1c30jfjhc0i.apps.googleusercontent.com";
 
-export const URL_PRODUCAO = "http://api-siand.vercel.app";
+
 
