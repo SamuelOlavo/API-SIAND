06c186892bef0736c27c6578009d926253f74540
SamuelOlavo <samuelobt@gmail.com>
2024-02-24 20:02:25 -0300

    fit: testando autenticação google

--- html/js/login.js
+++ html/js/login.js
@@ -14,7 +14,7 @@ window.onload = () => {
     document.getElementById('login').addEventListener('submit', handleSubmit);
 
 
-    const client = "771987966504-vri95o8gkbvprv8rc3l4d1c30jfjhc0i.apps.googleusercontent.com";
+    const client = "284670659527-lsq5agc4ap3pmapurrjenjmqo9k6b5gd.apps.googleusercontent.com";
 
     google.accounts.id.initialize({
       client_id: client,
